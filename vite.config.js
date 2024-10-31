9ba0bf9f73e080d40e91552485fe24ed1df69688
AadiSharma49 <aadi198555@gmail.com>
2024-10-31 11:51:18 +0530

    Initial Upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Job-Listing-React",
+  base:"/Job-Listing/",
   plugins: [react()],
 })
